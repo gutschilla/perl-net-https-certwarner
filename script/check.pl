2ed4dfb3ab3ae5cbcf6d6958db17f9ce5335f5c4
Martin Gutsch <gutsch@zwoelf.net>
2016-02-09 16:53:00 +0100

    return nonzero state upon error, add --quiet option

--- script/check.pl
+++ script/check.pl
@@ -5,19 +5,24 @@ BEGIN {
     use Moose;
     our $VSERION = 0.1;
     with 'MooseX::Getopt';
-    has 'host' => ( traits => [qw(Getopt)], cmd_aliases => 'h', is => 'rw', isa => 'Str',  required => 1 );
-    has 'port' => ( traits => [qw(Getopt)], cmd_aliases => 'p', is => 'rw', isa => 'Num',  default  => 443 );
+    has 'host'  => ( traits => [qw(Getopt)], cmd_aliases => 'h', is => 'rw', isa => 'Str',  required => 1 );
+    has 'port'  => ( traits => [qw(Getopt)], cmd_aliases => 'p', is => 'rw', isa => 'Num',  default  => 443 );
+    has 'quiet' => ( traits => [qw(Getopt)], cmd_aliases => 'q', is => 'rw', isa => 'Bool', default  => 0 );
     1;
 }
 
 use FindBin;
 use lib qq[$FindBin::Bin/../lib];
 
-my $options = Certwarner::ScriptOptions->new_with_options();
+our $options = Certwarner::ScriptOptions->new_with_options();
 
 use Net::HTTPS::Certwarner;
 use Try::Tiny;
 use feature 'say';
+
+sub ssay {
+    say shift if not $options->{quiet};
+}
 
 my $checker = Net::HTTPS::Certwarner->new(
     host => $options->{host},
@@ -26,13 +31,14 @@ my $checker = Net::HTTPS::Certwarner->new(
 my $message = $checker->check;
 
 if ( $message->{is_ok} ) {
-    say "certificate is fine today!";
+    ssay "certificate is fine today!";
     my $in_a_week = $checker->check( date_within => DateTime->now->add( days => 7 ) );
-    say  "...but it needs to be renewed soon!" if not $in_a_week->{is_ok};
+    ssay  "...but it needs to be renewed soon!" if not $in_a_week->{is_ok};
+    exit 0 if $in_a_week->{is_ok};
 }
 else {
-    say "certificate is out of date! " . $message->{error};
-    say "valid from: " . $message->{not_before}->dmy . ' till ' . $message->{not_after}->dmy;
-
+    ssay "certificate is out of date! " . $message->{error};
+    ssay "valid from: " . $message->{not_before}->dmy . ' till ' . $message->{not_after}->dmy;
 };
 
+exit 1;
